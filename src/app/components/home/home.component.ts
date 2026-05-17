3d3a74af478d0a0baf469b5a6a858cadc3100730
minhaz <minahmed17@gmail.com>
2026-05-18 01:15:31 +0600

    home page design and routing fixation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,13 @@
 // src/app/home/home.component.ts
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+  imports: [RouterModule],
 })
 export class HomeComponent { 
 
