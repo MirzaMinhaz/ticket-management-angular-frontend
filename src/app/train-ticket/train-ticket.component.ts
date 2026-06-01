30f744f82d43393b4940a516c7d3b27bb8b6b400
minhaz <minahmed17@gmail.com>
2026-06-01 23:59:38 +0600

    train ticket component

---
+++ src/app/train-ticket/train-ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-train-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './train-ticket.component.html',
+  styleUrl: './train-ticket.component.css'
+})
+export class TrainTicketComponent {
+
+}
